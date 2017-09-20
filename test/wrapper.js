d7de98d37559e69a11be538b398759e0a19eb465
Kevin Xi <kevin.xgr@gmail.com>
2017-09-20 22:08:00 +0800

    Test: impl an intruding tMemoize in case of dealing with anonymous functions.

--- test/wrapper.js
+++ test/wrapper.js
@@ -19,9 +19,26 @@ exports.cleanup = function () {
     process.stdout.write(`${Object.keys(tms).length}`);
 }
 
-exports.tMemoize = function (func) {
+exports.tMemoizeImmutable = function (func) {
     return function (...args) {
         let tag = `${func.name}-` + args.slice(0, -1).join('~');
         getTMemo(tag).push(func, ...args);
     }
+}
+
+exports.tMemoize = function (func) {
+    return function (...args) {
+        let id = genId(func);
+        let tag = `${id}-` + args.slice(0, -1).join('~');
+        getTMemo(tag).push(func, ...args);
+    }
+}
+
+let acc = 0;
+function genId(func) {
+    if (func.__tmemoid === undefined) {
+        func.__tmemoid = ++acc;
+    }
+
+    return func.__tmemoid;
 }
